05ceb6cf9abdfb6021375568ba5959c1ae1d6766
Aashrey Sharma <aashrey9sharma@gmail.com>
2022-05-13 20:48:50 -0400

    Added options to darken or lighten current page

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,43 @@
 import { convertHexColorToRgbColor } from "@create-figma-plugin/utilities"
 
-export function darkenPages() {
-  changeAllPageColors('222222')
+let DARK_HEX = '222222'
+let LIGHT_HEX = 'f5f5f5'
+
+export function darkenAllPages() {
+  changeAllPageColors(DARK_HEX)
   figma.closePlugin('🎉')
 }
 
-export function lightenPages() {
-  changeAllPageColors('eaeaea')
+export function lightenAllPages() {
+  changeAllPageColors(LIGHT_HEX)
   figma.closePlugin('🎉')
+}
+
+export function darkenCurrentPage() {
+  changeCurrentPageColor(DARK_HEX)
+  figma.closePlugin('🎉')
+}
+
+export function lightenCurrentPage() {
+  changeCurrentPageColor(LIGHT_HEX)
+  figma.closePlugin('🎉')
+}
+
+function changeCurrentPageColor(hexColor: string) {
+  figma.currentPage.backgrounds = [createBackgroundPaint(hexColor)]
 }
 
 function changeAllPageColors(hexColor: string) {
-  let backgroundPaint: Paint = {
-    type: 'SOLID',
-    color: convertHexColorToRgbColor(hexColor) as RGB
-  }
+  let backgroundPaint: Paint = createBackgroundPaint(hexColor)
   let pages: ReadonlyArray<PageNode> = figma.root.children
   for (let page of pages) {
     page.backgrounds = [backgroundPaint]
+  }
+}
+
+function createBackgroundPaint(hexColor: string): Paint {
+  return {
+    type: 'SOLID',
+    color: convertHexColorToRgbColor(hexColor) as RGB
   }
 }
